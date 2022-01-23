b8b2eaf49055791177ed48589b887465d546085e
GSingh777 <gursimratsingh1998@gmail.com>
2022-01-23 13:16:43 -0500

    First commit for static files

---
+++ src/api/index.js
@@ -0,0 +1,32 @@
+import { Data } from '@react-google-maps/api';
+import axios from 'axios';
+
+
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+const options = {
+  
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    
+  },
+  headers: {
+    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    'x-rapidapi-key': 'bfa396664dmshd1895cc40ac18f1p1dcc2djsnf30e6706315a'
+  }
+};
+
+
+export const getPlacesData = async () => {
+    try {
+        const {data: { data } } = await axios.get(URL, options);
+
+        return data;
+    } catch (error) {
+       console.log(error)
+    }
+}
